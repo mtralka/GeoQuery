0300e0e65bc0bc103ec87b5943f77e9ddcc356bd
Matthew Tralka <mtralka59@gmail.com>
2020-10-11 17:46:51 -0400

    full function search input

--- app/main.py
+++ app/main.py
@@ -15,21 +15,21 @@ WTF_CSRF_SECRET_KEY = 'CSRFSECRET'
 def index():
 	return render_template('home.html')
 
+
 @main.route('/search', methods=['GET', 'POST'])
 def search():
 
 	form = FlickrSearch()
 
-	#if form.is_submitted():
 	if form.validate_on_submit():
 
 		if form.validate() == False:
-			print('Failed Validation')
 			flash('Failed Validation')
 			return redirect(url_for('main.search'))
 
 		else:
 			data = request.form
+			print(data)
 			if 'lat' in data:
 				lat = data['lat']
 				print(lat)
@@ -38,15 +38,17 @@ def search():
 
 			flash('Success Validation')
 			return redirect(url_for('main.search'))
-	else:
-		print('something')
-		
 
+			# Submit to wrapper
+	
 	return render_template('search.html', form= form)
+
+
 
 # TODO adjust and implement
 @main.errorhandler(404)
 def page_not_found(error):
+
    return render_template('404.html', title = '404'), 404
 
 
