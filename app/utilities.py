c66c908dcba2d2f7ca1b4d5f7638053b7401526e
Matthew Tralka <mtralka59@gmail.com>
2020-12-05 13:58:19 -0500

    toFile delete Util

--- app/utilities.py
+++ app/utilities.py
@@ -42,18 +42,3 @@ def create_unique_id(length=5):
 def toGeo(df):
     """ convert dataframe to geodataframe """
     return gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude))
-
-
-def toFile(df, user, timestamp, format="GeoJSON"):
-
-    file_path = f"./response/{user}/{timestamp}"
-    Path(file_path).mkdir(parents=True, exist_ok=True)
-
-    if format == "GeoJSON":
-        df = toGeo(df)
-        df.to_file(file_path + "/master.geojson", driver="GeoJSON")
-    elif format == "CSV":
-        df.to_csv(file_path + "/master.csv", index=False)
-
-    elif format == "PICKLE":
-        df.to_pickle(file_path + "master.pkl")
